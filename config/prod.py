5631acb8be7f0cbb1fdd2539c53770a37c68a6bc
sanketsy <yelkarsanket@gmail.com>
2021-08-28 20:46:01 +0530

    Modified code for deployment to Heroku

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,11 @@
-DEBUG = True
-SECRET_KEY = 'topsecret'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/catalog_db'
+import os
+
+DEBUG = False
+SECRET_KEY = 'c9144660f9768975e97e06cb'
+
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = os.environ[uri]
 SQLALCHEMY_TRACK_MODIFICATIONS = False
