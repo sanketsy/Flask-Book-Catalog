711b6507dea3afe2f436c306f4c1b1543cd6e460
sanketsy <yelkarsanket@gmail.com>
2021-08-28 20:58:37 +0530

    Handling Integrity error

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from sqlalchemy import exc
 
 flask_app = create_app('prod')
 with flask_app.app_context():
-    db.create_all()
     try:
+        db.create_all()
         if not User.query.filter_by(user_name='harry').first():
             User.create_user(user='harry',
                              email='harry@test.com',
